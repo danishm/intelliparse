76191dc6c64cbff71e1da1cf58ff168e33a98476
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-04-05 21:22:54 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='IntelliParse',
+    version='0.1.0',
+    author='Danish Mujeeb',
+    author_email='danish@dsharpapps.com',
+    packages=['intelliparse', 'intelliparse.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/IntelliParse/',
+    license='LICENSE.txt',
+    description='A Python library to help parse product properties from simple English text',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
