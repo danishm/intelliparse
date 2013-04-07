69cfae0fb4b3dc4fa21e3c061fae8f9f70b16adf
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-04-06 21:06:33 -0400

    Added tests for storageSize

--- intelliparse/tests/test_electronics.py
+++ intelliparse/tests/test_electronics.py
@@ -4,9 +4,24 @@ from intelliparse import electronics
 
 class StorageSizeTests(unittest.TestCase):
     
-    def test_1(self):
-        s=electronics.parseStorageSize('I have a 32 TB disk')
-        self.assertEquals(s, Decimal('32768') )
+    def test_StorageSize(self):
+
+    	# Test storage size in the middle of the string
+        size=electronics.parseStorageSize('I have a 32 TB disk')
+        self.assertEquals(size, Decimal('32768') )
+
+        # Test storate size in the begining of the string
+        size=electronics.parseStorageSize('250 gb drives are the best')
+        self.assertEquals(size, Decimal('250') )
+
+        # Test storate size in the end of the string
+        size=electronics.parseStorageSize('I wish I had a drive with 50MB.')
+        self.assertEquals(size, Decimal('50')/Decimal('1024') )
+
+        # Test not mactching in the middle of the string
+        size=electronics.parseStorageSize('that drive is at most500 GB more than what I want')
+        self.assertIsNone(size)
+
     
     def test_2(self):
         pass
