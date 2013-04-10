bde3955e4c283e2069db85b2e6f5467ef068c056
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-04-09 23:20:52 -0400

    Fixed setup error

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1.0',
     author='Danish Mujeeb',
     author_email='danish@dsharpapps.com',
-    packages=['intelliparse', 'intelliparse.test'],
+    packages=['intelliparse', 'intelliparse.tests'],
     scripts=[],
     url='http://pypi.python.org/pypi/IntelliParse/',
     license='LICENSE.txt',
     description='A Python library to help parse product properties from simple English text',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[],
 )
