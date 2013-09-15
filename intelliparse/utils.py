0cb8b54eecaaebef759284e48d0f309a0a663562
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-09-15 17:42:28 -0400

    Add ability to deal with commas in the numeric figures

--- intelliparse/utils.py
+++ intelliparse/utils.py
@@ -4,6 +4,7 @@ A simple module to contain some utility functions for re-use
 
 def to_float(val):
     """Function to abstract out and centralize the parsing of a float"""
+    val=val.replace(',','')
     return float(val)
 
 def to_int(val):
